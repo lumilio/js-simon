4e28c9987694aa4a35e3988fda6d24aa29845363
lumilio <logistic-division@lumilium.com>
2021-10-27 14:41:05 +0200

    inserisco i numeri indovinati in un altro arrey

--- asset/js/main.js
+++ asset/js/main.js
@@ -8,18 +8,66 @@ Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto pr
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 Consigli del giorno:
 
-Info:
-L'alert non si chiude in automatico. Dovete chiuderla voi a mano cliccando su ok. */
+*/
+
+
+
+const numberBox = document.getElementById('container-game')
+const playButton = document.getElementById('button-game')
+const timerBox = document.getElementById('container-timer')
+const fineButton = document.getElementById('fine-game')
 
 const theNumbers = []
+const trueNumbers = []
+
+let seconds = 0;
+
+
+
+
 
 function randomInterval(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min)
-  }
+}
 
+
+let number='';
 for (let i = 0; i < 5; i++) {
-   let number = randomInterval(0, 100)
-   theNumbers.push(number)
+   let input = randomInterval(0, 100)
+   theNumbers.push(input)
+   number=`<div class='number-box'>${input}</div>`
+   numberBox.innerHTML += number   
 }
 
 console.log(theNumbers);
+
+
+
+
+for (let i = 0; i < theNumbers.length; i++) {
+    const input = Number(prompt('inserisci un numero che hai visto'));
+    if(theNumbers.includes(input)){
+        trueNumbers.push(input);
+    }  
+}
+
+console.log(trueNumbers);
+
+
+/* playButton.addEventListener('click', startGame )
+
+function startGame() {
+    playButton.classList.add('d-none')
+    numberBox.classList.add('d-none')
+    for (let i = 0; i < theNumbers.length; i++) {
+        const aswer = prompt('inserisci un numero che hai visto');
+        
+    }
+} */
+
+
+
+
+  
+
+/* setTimeout(myFunction, 30000) */
